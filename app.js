675de56d27d24679c45e7ac0eceb6ccdb4a1f31c
MistrDev <cwcorster123@gmail.com>
2021-01-07 08:51:14 -0600

    Learing the DOM with colt steele

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const allLinks = document.querySelectorAll('a');
 
 
 for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
+    link.style.color = 'rgb(0, 145, 134)';
     link.style.textDecorationColor = 'blue';
     link.style.textDecorationStyle = 'wavy'
 }
