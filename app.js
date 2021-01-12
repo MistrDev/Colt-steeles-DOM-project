07e02b1d3083c0442f65037213ebcd1ccc861120
MistrDev <cwcorster123@gmail.com>
2021-01-12 11:30:21 -0600

    just deleted a useless js line

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const allLinks = document.querySelectorAll('a');
 
 
 for (let link of allLinks) {
-    link.style.color = 'rgb(0, 145, 134)';
     link.style.textDecorationColor = 'purple';
     link.style.textDecorationStyle = 'wavy'
 }
