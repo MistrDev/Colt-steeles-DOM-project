ccdfd6c12ecc95474fdea9fb888621b6e5de0f26
MistrDev <cwcorster123@gmail.com>
2021-01-14 10:01:46 -0600

    just messing around with styles using the DOM

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const allLinks = document.querySelectorAll('a');
 
 for (let link of allLinks) {
     link.style.textDecorationColor = 'purple';
-    link.style.textDecorationStyle = 'wavy'
+    link.style.textDecorationStyle = 'dotted'
 }
 
 
