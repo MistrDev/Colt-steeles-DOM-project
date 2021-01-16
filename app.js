fd944920d7072c9b79c8ea4c0393ad4373e6a1ad
MistrDev <cwcorster123@gmail.com>
2021-01-16 17:22:06 -0600

    messing around with the dom and css

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const allLinks = document.querySelectorAll('a');
 
 for (let link of allLinks) {
     link.style.textDecorationColor = 'purple';
-    link.style.textDecorationStyle = 'dotted'
+    link.style.textDecorationStyle = 'dotted';
+    link.style.color = 'green';
 }
