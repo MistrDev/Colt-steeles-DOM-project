d58efb8012511c79bc3a6280d6061ade3710191d
MistrDev <cwcorster123@gmail.com>
2021-01-17 09:37:00 -0600

    learned how to add event listeners in the DOM in my js file!

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ for (let link of allLinks) {
     link.style.textDecorationStyle = 'dotted';
     link.style.color = 'green';
 }
+
+const button = document.querySelector('#v1');
+
+button.addEventListener('click', () => {
+    alert('This is way better than Inline!')
+})
