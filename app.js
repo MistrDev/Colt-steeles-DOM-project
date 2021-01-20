3fc00dd1317c46eefa572b92e65747c3c6953543
MistrDev <cwcorster123@gmail.com>
2021-01-20 08:51:58 -0600

    mission was succesful. I now have tiny rgb buttons!

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ for (let link of allLinks) {
     link.style.color = 'green';
 }
 
+const randomColor = () => {
+    const R = Math.floor(Math.random() * 256);
+    const G = Math.floor(Math.random() * 256);
+    const B = Math.floor(Math.random() * 256);
+      return `rgb(${R}, ${G}, ${B})`;  
+  }
+
+
+const buttons = document.querySelectorAll('button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function() {
+        button.style.backgroundColor = randomColor();
+    })
+}
