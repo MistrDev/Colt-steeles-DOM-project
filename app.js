0f0631d7d04e61ae3394e56d1be9f8cacf2a6711
MistrDev <cwcorster123@gmail.com>
2021-01-23 08:46:21 -0600

    changed my rgb var to lower case

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ for (let link of allLinks) {
 }
 
 const randomColor = () => {
-    const R = Math.floor(Math.random() * 256);
-    const G = Math.floor(Math.random() * 256);
-    const B = Math.floor(Math.random() * 256);
-      return `rgb(${R}, ${G}, ${B})`;  
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+      return `rgb(${r}, ${g}, ${b})`;  
   }
 
 
