3f78e2812edba8c8e91118f9bd6b02c5978a259c
MistrDev <cwcorster123@gmail.com>
2021-01-31 08:14:09 -0600

    changed the color of the text of all links

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const allLinks = document.querySelectorAll('a');
 for (let link of allLinks) {
     link.style.textDecorationColor = 'purple';
     link.style.textDecorationStyle = 'dotted';
-    link.style.color = 'green';
+    link.style.color = 'blue';
 }
 
 
